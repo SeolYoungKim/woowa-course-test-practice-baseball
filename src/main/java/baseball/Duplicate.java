022f9df5d5de04489ae7d3ea35afcfad046dee6a
hyeonjaez <jhjh0022@naver.com>
2022-11-27 22:26:03 +0900

    feat(Duplicate): 중복 하ë되는 숫자 추출 하는 기능 구현

---
+++ src/main/java/baseball/Duplicate.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public class Duplicate {
+
+    public List<Integer> getDuplicateNumber(List<Integer> computer, List<Integer> player) {
+        return computer.stream()
+                .filter(number -> player.stream()
+                        .anyMatch(Predicate.isEqual(number)))
+                .collect(Collectors.toList());
+    }
+}
