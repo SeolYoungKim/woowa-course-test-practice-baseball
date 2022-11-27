ddd5851c46096be0b26084be3cb1bb79880d60c3
hyeonjaez <jhjh0022@naver.com>
2022-11-27 21:21:08 +0900

    feat(Verification): ê 입력받은 값 검증하는 기능 구현

--- src/main/java/baseball/Verification.java
+++ src/main/java/baseball/Verification.java
@@ -1,2 +1,37 @@
-package baseball;public class Verification {
+package baseball;
+
+import java.util.*;
+
+public class Verification {
+
+    TypeChange typeChange = new TypeChange();
+
+
+    public void verifyThreeNumber(String number) {
+        if (!(number.length() == 3)) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    public void verify1Between9(String number) {
+        for (int numbers : typeChange.splitNumberList(number)) {
+            if (!(1 <= numbers && numbers <= 9)) {
+                throw new IllegalArgumentException("");
+            }
+        }
+    }
+
+    public void verifyDuplicateNumber(String number) {
+        if (!(typeChange.splitNumberList(number).size() == typeChange.splitNumberSet(number).size())) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+    public void verify1Or2(String number) {
+        if (!(Objects.equals(number, "1") || Objects.equals(number, "2"))) {
+            throw new IllegalArgumentException("");
+        }
+    }
+
+
 }
