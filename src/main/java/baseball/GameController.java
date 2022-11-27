6fef91729e6421659b5aa16196988755cf5a4304
hyeonjaez <jhjh0022@naver.com>
2022-11-27 22:35:10 +0900

    feat(GameController): 게임 진행 해주는 기능 구현

---
+++ src/main/java/baseball/GameController.java
@@ -0,0 +1,72 @@
+package baseball;
+
+import java.util.List;
+import java.util.Objects;
+
+public class GameController {
+
+    private final InputView inputView;
+    private final OutputView outputView;
+    private final RandomNumber randomNumber = new RandomNumber();
+    private final Judgement judgement = new Judgement(new Duplicate());
+
+    GameController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void startGame() {
+        outputView.startGame();
+        startBaseball();
+    }
+
+    public void startBaseball() {
+        makeAnswerNumber();
+        retryOrFinish();
+    }
+
+    public void makeAnswerNumber() {
+        List<Integer> answerNumber = randomNumber.makeAnswer();
+        baseball(answerNumber);
+    }
+
+    public void baseball(List<Integer> answerNumber) {
+        List<Integer> player = getNumber();
+        h(answerNumber, player);
+    }
+
+    public List<Integer> getNumber() {
+        outputView.askNumber();
+        return inputView.readPlayerNumber();
+    }
+
+    public void h(List<Integer> answerNumber, List<Integer> player) {
+        int strike = judgement.strike(answerNumber, player);
+        int ball = judgement.ball(answerNumber, player);
+        outputView.informResult(ball, strike);
+        checkStrike(strike, answerNumber);
+    }
+
+    public void checkStrike(int strike, List<Integer> answerNumber) {
+        if (strike != 3) {
+            baseball(answerNumber);
+        }
+    }
+
+    public void retryOrFinish() {
+        String retry = getRetryNumber();
+        if (checkRetry(retry)) {
+            startBaseball();
+        }
+    }
+
+    public String getRetryNumber() {
+        outputView.correctAnswer();
+        outputView.askRetry();
+        return inputView.readRetry();
+    }
+
+    public boolean checkRetry(String d) {
+        return Objects.equals(d, "1");
+    }
+}
