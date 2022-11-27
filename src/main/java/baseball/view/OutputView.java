9f0600088504e1ca65dc512105338e5ab9ae24e1
SeolYoungKim <nasur4da@gmail.com>
2022-11-27 19:20:09 +0900

    feat(OutputView): 출력 관련 기능 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+public class OutputView {
+
+    // 게임 시작 문구를 출력한다.
+    public void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    // 입력한 수에 대한 결과를 볼, 스트라이크 개수로 표시한다.
+    public void printResults() {
+        // 결과를 받고, 결과를 포맷에 맞게 출력!
+    }
+
+    // 모두 맞췄을 경우 결과 출력
+    public void printEndMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
