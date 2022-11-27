2d9e129f09e74a308d8a3a97f7ef4f25aa5a27aa
SeolYoungKim <nasur4da@gmail.com>
2022-11-27 23:56:07 +0900

    refactor(OutputViewFormatter): 그냥 사용하던 문자열 상수화

--- src/main/java/baseball/view/OutputViewFormatter.java
+++ src/main/java/baseball/view/OutputViewFormatter.java
@@ -5,22 +5,27 @@ import java.util.StringJoiner;
 
 public class OutputViewFormatter {
 
+    private static final String NEITHER_STRIKE_NOR_BALL = "낫싱";
+    private static final String BLANK = " ";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+
     String resultFormat(ComparisonResult comparisonResult) {
         if (comparisonResult.isNothing()) {
-            return "낫싱";
+            return NEITHER_STRIKE_NOR_BALL;
         }
 
         return ballAndStrikeFormat(comparisonResult);
     }
 
     private String ballAndStrikeFormat(ComparisonResult comparisonResult) {
-        StringJoiner stringJoiner = new StringJoiner(" ");
+        StringJoiner stringJoiner = new StringJoiner(BLANK);
         if (comparisonResult.hasMoreThanOneBall()) {
-            stringJoiner.add(comparisonResult.ballCount() + "볼");
+            stringJoiner.add(comparisonResult.ballCount() + BALL);
         }
 
         if (comparisonResult.hasMoreThanOneStrike()) {
-            stringJoiner.add(comparisonResult.strikeCount() + "스트라이크");
+            stringJoiner.add(comparisonResult.strikeCount() + STRIKE);
         }
         return stringJoiner.toString();
     }
